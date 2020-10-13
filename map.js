23ed20d04162565771a89b15318ee7720776bba7
fahimmeadahmed <fahimmeadahmed@gmail.com>
2020-10-13 13:21:31 +0600

    js map,filter method

---
+++ map.js
@@ -0,0 +1,8 @@
+*/const numbers = [1, -1, 2, 3];
+
+const filtered = numbers.filter(n => n >= 0 );
+
+const items = filtered.map(n => '<li>' + n + '<li>');
+
+console.log(items);*/
+console.log('hello');
