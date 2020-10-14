3fcf2e7681da3f1d6c03107982f092a04ab89127
fahimmeadahmed <fahimmeadahmed@gmail.com>
2020-10-14 11:47:05 +0600

    state

--- React/hello-world/src/App.js
+++ React/hello-world/src/App.js
@@ -4,17 +4,19 @@ import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
+import Messege from './components/Messege'
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Mead" degree="CSE" > <p> this is  engineers property </p> </Greet>
+      {/* <Greet name="Mead" degree="CSE" > <p> this is  engineers property </p> </Greet>
       <Greet name="Ahmed" degree="CSSE" > <button>Action</button></Greet>
       <Greet name="Fahim" degree="SE" />
       <Welcome name="Mead" degree="CSE"></Welcome>
       <Welcome name="Ahmed" degree="CSSE"></Welcome>
-      <Welcome name="Fahim" degree="SE"></Welcome>
+      <Welcome name="Fahim" degree="SE"></Welcome> */}
       {/*<Hello />*/}
+      <Messege />
     </div>
   );
 }
---
+++ React/hello-world/src/components/Messege.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+class Messege extends Component {
+    constructor(){
+        super()
+        this.state = {
+            messege: 'Welcome Visitors'
+        }
+    }
+    changeMessege(){
+        this.setState({
+            messege: 'Thank you for subscribing'
+        })
+    }
+
+    render () {
+        return(
+            <div>
+            <h1> {this.state.messege} </h1>
+
+            <button onClick={() => this.changeMessege()}> Subscribe </button>
+        </div>
+        )
+    }
+}
+
+export default Messege
